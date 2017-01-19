fa129b3e6cfc318f98f51b9d89dbf6d821d6a852
Sixten Hilborn <hilborn90@gmail.com>
2017-01-19 07:17:40 +0100

    Add CI

---
+++ test_package/example.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "RakPeerInterface.h"
+
+#define MAX_CLIENTS 10
+#define SERVER_PORT 60000
+
+int main(void)
+{
+	char str[512];
+	RakNet::RakPeerInterface *peer = RakNet::RakPeerInterface::GetInstance();
+	bool isServer;
+
+	printf("(C) or (S)erver?\n");
+	//gets(str);
+	if ((str[0] == 'c') || (str[0] == 'C'))
+	{
+		SocketDescriptor sd;
+		peer->Startup(1, &sd, 1);
+		isServer = false;
+	}
+	else {
+		SocketDescriptor sd(SERVER_PORT, 0);
+		peer->Startup(MAX_CLIENTS, &sd, 1);
+		isServer = true;
+	}
+
+
+	// TODO - Add code body here
+
+	RakNet::RakPeerInterface::DestroyInstance(peer);
+
+	return 0;
+}
